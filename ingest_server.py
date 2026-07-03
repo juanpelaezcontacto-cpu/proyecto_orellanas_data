fac39c9541349ebd1a2d33ba0c691574fe5626dc
juanpelaezcontacto-cpu <juanpelaezcontacto@gmail.com>
2026-07-03 12:46:00 -0500

    Creado servidor Flask en Python para recibir telemetría de la ESP32

---
+++ ingest_server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/telemetria', methods=['POST'])
+def recibir_datos():
+    datos = request.json  # Aquí recibes el JSON de la ESP32
+    print("Datos recibidos:", datos)
+    
+    # Aquí es donde más adelante conectarás SQL para guardarlos
+    # Por ahora, solo los guardaremos en un archivo de texto local
+    with open("registro_datos.txt", "a") as archivo:
+        archivo.write(f"{datos}\n")
+        
+    return jsonify({"status": "success"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) # El 0.0.0.0 permite que la ESP32 lo encuentre en la red
