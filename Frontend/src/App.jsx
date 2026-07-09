890e1347fbb1941f269544385b97d88d10832c7e
juanpelaezcontacto-cpu <juanpelaezcontacto@gmail.com>
2026-07-09 14:53:29 -0500

    grafica co2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -165,7 +165,7 @@ function App() {
             </ResponsiveContainer>
           </div>
         </div>
-
+        {/*Grafica Humedad*/}
         <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '14px', boxShadow: '0 2px 4px rgba(0,0,0,0.02)' }}>
           <h3 style={{ fontSize: '16px', fontWeight: 'bold', margin: '0 0 16px 0', color: '#2d3748' }}>Historial de Humedad (%)</h3>
           <div style={{ width: '100%', height: 260 }}>
@@ -180,6 +180,22 @@ function App() {
                 <Line type="monotone" dataKey="hum_int_inf" name="Interior Inf." stroke="#3182ce" strokeWidth={2.5} dot={false} />
                 <Line type="monotone" dataKey="hum_int_sup" name="Interior Sup." stroke="#805ad5" strokeWidth={2.5} dot={false} />
                 <Line type="monotone" dataKey="hum_ext" name="Exterior" stroke="#a0aec0" strokeWidth={1.5} strokeDasharray="4 4" dot={false} />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+        {/*Grafica CO2*/}
+        <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '14px', boxShadow: '0 2px 4px rgba(0,0,0,0.02)' }}>
+          <h3 style={{ fontSize: '16px', fontWeight: 'bold', margin: '0 0 16px 0' }}>Temperatura</h3>
+          <div style={{ width: '100%', height: 260 }}>
+            <ResponsiveContainer>
+              <LineChart data={datosClima}>
+                <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#edf2f7" />
+                <XAxis dataKey="hora" tick={{fontSize: 11}} stroke="#a0aec0" />
+                <YAxis domain={['auto', 'auto']} tick={{fontSize: 11}} stroke="#a0aec0" />
+                <Tooltip />
+                <Legend verticalAlign="top" height={32}/>
+                <Line type="monotone" dataKey="co2_inf" name="Co2" stroke="#e53e3e" strokeWidth={2.5} dot={false} />
               </LineChart>
             </ResponsiveContainer>
           </div>
